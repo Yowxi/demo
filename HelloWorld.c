694d16dd5c8e293f9f4a73b22ada629f45b8bf76
Yowxi <263830681@qq.com>
2026-04-30 18:39:06 +0800

    提交第二个版本：实现键盘输入俞显示功能

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello, World!\n");
+    char input[100];
+    printf("请输入任意内容：");
+    fgets(input, sizeof(input), stdin);
+    printf("你从键盘输入的内容是这个哦：%s", input);
     return 0;
 }
